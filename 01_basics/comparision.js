766e64078159c9c39ed66bae73412077c4818602
Roshanichaudhary <149548219+Roshanichaudhary@users.noreply.github.com>
2025-01-29 08:04:48 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,17 @@
+console.log("2">1); //t=true
+console.log("02">1);t
+console.log(null>0);f
+
+console.log(null==0);f
+console.log(null>=0);t
+console.log(undefined==0);//f
+console.log(undefined>0);//f
+console.log(undefined<0);//f
+console.log("2"==2);//only check conversion
+console.log("2"===2);//check conversion and datatype
+
+
+
+
+
+
