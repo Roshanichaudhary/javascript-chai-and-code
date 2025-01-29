d07a10e1600039d00f0392fedd1ae80ef7f58f08
Roshanichaudhary <149548219+Roshanichaudhary@users.noreply.github.com>
2025-01-29 07:24:43 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,6 +22,57 @@
  let stringNumber=String(someNumber)
  console.log(stringNumber);
  console.log(typeof stringNumber);
+
+
+
+ //************************* operation ******************/
+ let value=3
+ let negvalue=-value
+ console.log(negvalue);
+
+
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**2);
+//  console.log(2**3);
+//  console.log(2/3);
+//  console.log(2%3);
+ 
+ 
+ let str1="hello"
+ let str2="hitesh"
+ let str3=str1+str2
+ console.log(str3);// hello hitesh//
+
+ console.log("1"+2);//12
+ console.log(1 +"2");//12
+ console.log("1"+2+2);//122
+ console.log(1+2+"2");//32
+ console.log(+ true);// 1
+ console.log(+"");//0
+
+ let gameCounter=100
+ gameCounter++;//postfix// READ BY PREFIX AND POSTFIX JS MDN
+ ++gameCounter;// prefix 
+ console.log(gameCounter);
+
+ //link to study=>ecmascript
+ https://262.ecma-international.org/#sec-abstract-operations
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
  
  
